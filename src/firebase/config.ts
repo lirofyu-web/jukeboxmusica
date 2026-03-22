6b593871a995e746c205aa53337e351a3fd2711a
pedro <maniaveiculos2014@gmail.com>
2026-03-21 21:55:04 -0300

    feat: Deploy Seguro Firebase Hosting (Static) - Simplificado

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,9 @@
 
 export const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyAnJxtaaEsoQs8zPFMFTZpff71hv2blpcg",
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "studio-5185962361-c20ec.firebaseapp.com",
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "studio-5185962361-c20ec",
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "studio-5185962361-c20ec.appspot.com",
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "",
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || ""
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "studio-5185962361-c20ec.firebasestorage.app",
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "211922610764",
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:211922610764:web:b4d565e34c1329e73c22e9"
 };
